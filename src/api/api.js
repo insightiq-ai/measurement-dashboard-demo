2a76d52f25bece8a970477a2477470000e74e431
abhi-insightiq <abhinand.kr@insightiq.ai>
2024-06-14 15:13:03 +0530

    Attempting short.io

--- src/api/api.js
+++ src/api/api.js
@@ -1,194 +1,197 @@
 import { getBasicAuthInstance, getShopifyAuthInstance } from "./axiosInstance";
-import { CREATOR_SPLIT } from "../utils/constants";
+import { CREATOR_SPLIT, CREATOR_AAKASH, CREATOR_DHRUV, CREATOR_MIKE } from "../utils/constants";
 import { currencyFormatter, percentFormatter } from "../utils/util";
-
+import { isEmpty } from "../utils/util";
 const axios = require("axios");
 
-export async function getPromocodeAnalytics({ warehouse, storeId }) {
-  // const { clientId, clientSecret } = getClientIDAndSecret();
-  const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
-
-  try {
-    const response = await api.get(`v1/measurement/stores/${storeId}/discounts/analytics`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+export async function getPromocodeAnalytics({ warehouse, storeId, discountId }) {
+    // const { clientId, clientSecret } = getClientIDAndSecret();
+    const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
+    let url = `v1/measurement/warehouses/${warehouse}/stores/${storeId}/discounts/analytics`;
+    !isEmpty(discountId) && (url += `?discount_id=${discountId}`);
+    try {
+        const response = await api.get(url);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function getAttributionStatistics() {
-  const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
-  try {
-    const response = await api.get(`v1/measurement/attribution/statistics`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+    const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
+    try {
+        const response = await api.get(`v1/measurement/attribution/statistics`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function getDashboardLinkMetrics() {
-  const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
+    const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
 
-  try {
-    const response = await api.get(`v1/measurement/dashboard/links/metrics`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const response = await api.get(`v1/measurement/dashboard/links/metrics`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function getUsers({ limit, offset }) {
-  const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
+    const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
 
-  async function addEventTimestamp(users) {
-    // Fetch events for all users concurrently
-    const eventsPromises = users.data.map((user) =>
-      getUserEvents({ userId: user.id, limit: 1 }).catch((error) => {
-        console.log(`Failed to fetch events for user ${user.id}:`, error);
-        return null; // Return null or default object if an individual request fails
-      })
-    );
-    try {
-      // Await all event data requests
-      const eventsResults = await Promise.all(eventsPromises);
-      // Combine user data with event data
-      const enrichedUsers = users.data.map((user, index) => {
-        return {
-          ...user,
-          event_timestamp: eventsResults[index]?.data[0]?.event_timestamp ?? null,
-        };
-      });
-      return enrichedUsers;
-    } catch (error) {
-      console.log("Failed to fetch event_timestamp for all users:", error);
-      return users.data;
+    async function addEventTimestamp(users) {
+        // Fetch events for all users concurrently
+        const eventsPromises = users.data.map((user) =>
+            getUserEvents({ userId: user.id, limit: 1 }).catch((error) => {
+                console.log(`Failed to fetch events for user ${user.id}:`, error);
+                return null; // Return null or default object if an individual request fails
+            })
+        );
+        try {
+            // Await all event data requests
+            const eventsResults = await Promise.all(eventsPromises);
+            // Combine user data with event data
+            const enrichedUsers = users.data.map((user, index) => {
+                return {
+                    ...user,
+                    event_timestamp: eventsResults[index]?.data[0]?.event_timestamp ?? null,
+                };
+            });
+            return enrichedUsers;
+        } catch (error) {
+            console.log("Failed to fetch event_timestamp for all users:", error);
+            return users.data;
+        }
     }
-  }
 
-  try {
-    const response = await api.get(`v1/measurement/users?list_anonymous_users=true&list_users_with_no_events=true&limit=${limit}&offset=${offset}`);
-    const users = response.data;
+    try {
+        const response = await api.get(`v1/measurement/users?list_anonymous_users=true&list_users_with_no_events=true&limit=${limit}&offset=${offset}`);
+        const users = response.data;
 
-    return users.data;
-    // return await addEventTimestamp(users);
-  } catch (error) {
-    console.log(error);
-  }
+        return users.data;
+        // return await addEventTimestamp(users);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function getUserById({ userId }) {
-  const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
-  try {
-    const response = await api.get(`v1/measurement/users/${userId}`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+    const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
+    try {
+        const response = await api.get(`v1/measurement/users/${userId}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function getUserEvents({ userId, limit, offset }) {
-  const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
-  try {
-    const response = await api.get(`v1/measurement/attribution/events?user_id=${userId}&limit=${limit}&offset=${offset}`);
-    return response.data.data;
-  } catch (error) {
-    console.log(error);
-  }
+    const api = getBasicAuthInstance(process.env.REACT_APP_CLIENT_ID_PROD, process.env.REACT_APP_CLIENT_SECRET_PROD, "https://api.insightiq.ai");
+    try {
+        const response = await api.get(`v1/measurement/attribution/events?user_id=${userId}&limit=${limit}&offset=${offset}`);
+        return response.data.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function shortIoLinkAnalytics({ linkId }) {
+    const url = `https://statistics.short.io/statistics/link/${linkId}`;
+    const options = {
+        headers: {
+            'Authorization': process.env.REACT_APP_SHORT_IO_API_KEY,
+            'accept': '*/*'
+        },
+        params: {
+            period: 'total',
+            tz: 'UTC'
+        }
+    };
+
+    try {
+        const response = await axios.get(url, options);
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.error('Error making API request:', error);
+        return null;
+    }
 }
 
 export async function getCreatorsData() {
-  return new Promise((resolve, reject) => {
-    console.log(CREATOR_SPLIT);
-    resolve(
-      CREATOR_SPLIT.map((creator, index) => {
-        return {
-          id: index,
-          thumbnail: creator.icon,
-          name: creator.title,
-          utm_clicks: 9999,
-          creator_cost: currencyFormatter.format(creator.metric),
-          total_sales: 20,
-          roi: percentFormatter.format(9999 / creator.metric),
-          platforms: ["YouTube", "TikTok"],
-        };
-      })
-    );
-    // const data = [{
-    //     thumbnail: CREATOR_SPLIT[0].icon,
-    //     name: CREATOR_SPLIT[0].title,
-    //     utm_clicks: 100,
-    //     creator_cost: currencyFormatter.format(CREATOR_SPLIT[0].metric),
-    //     total_sales: 9999,
-    //     roi: 9999 / CREATOR_SPLIT[0].metric,
-    //     platforms: ['YouTube', 'TikTok'],
-    // }, {
-    //     thumbnail: CREATOR_SPLIT[1].icon,
-    //     name: CREATOR_SPLIT[1].title,
-    //     utm_clicks: 200,
-    //     creator_cost: currencyFormatter.format(CREATOR_SPLIT[1].metric),
-    //     total_sales: 9999,
-    //     roi: 9999 / CREATOR_SPLIT[1].metric,
-    //     platforms: ['Instagram', 'Twitter'],
-    // }, {
-    //     thumbnail: CREATOR_SPLIT[2].icon,
-    //     name: CREATOR_SPLIT[2].title,
-    //     utm_clicks: 300,
-    //     creator_cost: currencyFormatter.format(CREATOR_SPLIT[2].metric),
-    //     total_sales: 9999,
-    //     roi: 9999 / CREATOR_SPLIT[2].metric,
-    //     platforms: ['YouTube', 'Instagram'],
-    // }, {
-    //     thumbnail: CREATOR_SPLIT[3].icon,
-    //     name: CREATOR_SPLIT[3].title,
-    //     utm_clicks: 400,
-    //     creator_cost: currencyFormatter.format(CREATOR_SPLIT[3].metric),
-    //     total_sales: 9999,
-    //     roi: 9999 / CREATOR_SPLIT[3].metric,
-    //     platforms: ['TikTok', 'Twitter'],
-    // }];
-    // resolve(data);
-  });
+    return new Promise((resolve, reject) => {
+
+        const creatorToLinkIdMapping = {};
+        creatorToLinkIdMapping[CREATOR_DHRUV] = [process.env.REACT_APP_EXTERNAL_LINK_ID_1, process.env.REACT_APP_EXTERNAL_LINK_ID_2];
+        creatorToLinkIdMapping[CREATOR_AAKASH] = [process.env.REACT_APP_EXTERNAL_LINK_ID_3];
+        creatorToLinkIdMapping[CREATOR_MIKE] = [process.env.REACT_APP_EXTERNAL_LINK_ID_4];
+
+        function getUtmClicksForCreator(creator) {
+            return creatorToLinkIdMapping[creator].reduce(async (acc, linkId) => {
+                const linkAnalytics = await shortIoLinkAnalytics({ linkId });
+                return acc + linkAnalytics['totalClicks'];
+            }, 0);
+        }
+
+        resolve(
+            CREATOR_SPLIT.map((creator, index) => {
+                const { icon, title, metric } = creator;
+                return {
+                    id: index,
+                    thumbnail: icon,
+                    name: title,
+                    utm_clicks: 100, // getUtmClicksForCreator(title),
+                    creator_cost: currencyFormatter.format(metric),
+                    total_sales: 20,
+                    roi: percentFormatter.format(9999 / metric),
+                    platforms: ["YouTube", "TikTok"],
+                };
+            })
+        );
+    });
 }
 
 export async function getAllOrdersFromShopify() {
-  const api = getShopifyAuthInstance("https://proxy.cors.sh/https://www.demoshoes.shop/admin/api/2023-10");
-  try {
-    const response = await api.get(`/orders/count.json?status=any`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+    const api = getShopifyAuthInstance("https://proxy.cors.sh/https://www.demoshoes.shop/admin/api/2023-10");
+    try {
+        const response = await api.get(`/orders/count.json?status=any`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function getCountOfAbondonedCheckout() {
-  const api = getShopifyAuthInstance("https://proxy.cors.sh/https://www.demoshoes.shop/admin/api/2023-10");
-  try {
-    const response = await api.get(`/checkouts.json`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+    const api = getShopifyAuthInstance("https://proxy.cors.sh/https://www.demoshoes.shop/admin/api/2023-10");
+    try {
+        const response = await api.get(`/checkouts.json`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function getTotalOrderPerAUID(auid) {
-  const url = "https://proxy.cors.sh/" + "https://orderops.demoshoes.shop";
-  var myHeaders = new Headers();
-  myHeaders.append("X-PHYLLO-PSK", "PHYLLOISKING");
-  myHeaders.append("API-TYPE", "AUID2ORDERDETAILS");
-  myHeaders.append("AUID", auid);
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("x-cors-api-key", process.env.REACT_APP_CORSSH_TOKEN);
+    const url = "https://proxy.cors.sh/" + "https://orderops.demoshoes.shop";
+    var myHeaders = new Headers();
+    myHeaders.append("X-PHYLLO-PSK", "PHYLLOISKING");
+    myHeaders.append("API-TYPE", "AUID2ORDERDETAILS");
+    myHeaders.append("AUID", auid);
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("x-cors-api-key", process.env.REACT_APP_CORSSH_TOKEN);
 
-  var raw = JSON.stringify({});
+    var raw = JSON.stringify({});
 
-  var requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  };
+    var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
 
-  return fetch(url, requestOptions)
-    .then((response) => response.json())
-    .catch((error) => console.log("error", error));
+    return fetch(url, requestOptions)
+        .then((response) => response.json())
+        .catch((error) => console.log("error", error));
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,8 +10,12 @@ export const CREATORS = 'Creators';
 export const TOTAL_CREATOR_COST = 2000;
 export const NUMBER_OF_CREATORS = 3;
 
+export const CREATOR_AAKASH = 'Aakash';
+export const CREATOR_DHRUV = 'Dhruv';
+export const CREATOR_MIKE = 'Mike';
+
 export const CREATOR_SPLIT = [
-    { icon: <Icons.youtube_demo/>, title: 'Aakash', metric: TOTAL_CREATOR_COST / 2 },
-    { icon: <Icons.instagram_demo/>, title: 'Dhruv', metric:  TOTAL_CREATOR_COST / 4 },
-    { icon: <Icons.twitter_demo/>, title: 'Mike', metric:  TOTAL_CREATOR_COST / 4 }
+    { icon: <Icons.youtube_demo/>, title: CREATOR_DHRUV, metric: TOTAL_CREATOR_COST / 2 },
+    { icon: <Icons.instagram_demo/>, title: CREATOR_AAKASH, metric:  TOTAL_CREATOR_COST / 4 },
+    { icon: <Icons.twitter_demo/>, title: CREATOR_MIKE, metric:  TOTAL_CREATOR_COST / 4 }
 ];
