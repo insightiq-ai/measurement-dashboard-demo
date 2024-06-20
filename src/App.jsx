612487838c7cac82df54d234523b52c05df324f5
abhi-insightiq <abhinand.kr@insightiq.ai>
2024-06-20 11:49:54 +0530

    Removing warehouse and other unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,15 @@
-import React, { useReducer } from 'react';
-import { CssBaseline, Typography } from '@mui/material';
-import PerformancePage from "./pages/PerformancePage/PerformancePage";
-import { WarehouseContext } from "./storage/context/index";
-import { DEFAULT_WAREHOUSE_STATE, warehouseReducer } from "./storage/reducers";
+import React from 'react';
+import { CssBaseline } from '@mui/material';
 import { Route, Routes } from "react-router-dom";
 import MeasurementDemoRoutes from "./routes/MeasurementDemoRoutes";
 
-function Home() {
-    return (
-        <Typography variant="h5" component="h2">
-            Home Page
-        </Typography>
-    );
-}
-
-function About() {
-    return (
-        <Typography variant="h5" component="h2">
-            About Page
-        </Typography>
-    );
-}
-
 function App() {
-    const [warehouse, dispatchWarehouse] = useReducer(warehouseReducer, DEFAULT_WAREHOUSE_STATE);
-
     return (
         <div className={'div-app'}>
             <CssBaseline/>
-            <WarehouseContext.Provider value={{ warehouse, dispatchWarehouse }}>
-                <Routes>
-                    <Route path="*" element={<MeasurementDemoRoutes />} />
-                </Routes>
-            </WarehouseContext.Provider>
+            <Routes>
+                <Route path="*" element={<MeasurementDemoRoutes/>}/>
+            </Routes>
         </div>
     );
 }
--- src/storage/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { DEFAULT_WAREHOUSE_STATE, warehouseReducer } from "./warehouseReducer";
-
-export {
-    warehouseReducer,
-    DEFAULT_WAREHOUSE_STATE,
-};
